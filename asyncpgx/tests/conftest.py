44f3f92022e265f05bb4eb966d4abfbe9b7ced90
laukhin <laukhin97@gmail.com>
2020-09-30 01:19:31 +0300

    Fight for 3.5

--- asyncpgx/tests/conftest.py
+++ asyncpgx/tests/conftest.py
@@ -10,7 +10,7 @@ POSTGRES_DSN: str = os.getenv('POSTGRES_TEST_DSN', 'postgresql://127.0.0.1:5432'
 
 
 @pytest.fixture
-async def postgres_connection() -> connection_module.ConnectionX:  # type: ignore
+async def postgres_connection():
     """Fixture which establishes the connection, creates the test table and
     cleans everything on scope close."""
     connection: connection_module.ConnectionX = await connection_module.connect(POSTGRES_DSN)
