10e82344d5e2547ca042fd504e83616ce2dfed23
Vladislav Laukhin <38098983+laukhin@users.noreply.github.com>
2021-12-01 13:49:37 +0300

    Bump copyright and release.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,11 +20,11 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'asyncpgx'
-copyright = '2020, Vladislav Laukhin'
+copyright = '2021, Vladislav Laukhin'
 author = 'Vladislav Laukhin'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.1.4'
 
 
 # -- General configuration ---------------------------------------------------
